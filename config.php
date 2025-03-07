ede397da240c8b38de92ea1deba40a931de33d7e
Hai Anh <nguyenhai.anh6599@gmail.com>
2025-03-07 17:03:09 +0700

    Update giao dien, modify giao dien categories theo database của index.php

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+$servername = "localhost:3307"; // Hoặc địa chỉ IP của máy chủ MySQL
+$username = "root"; // Thay bằng username của bạn
+$password = "12345678"; // Thay bằng password của bạn
+$database = "ecommerce"; // Tên database
+
+// Kết nối MySQL với MySQLi
+try {
+    $conn = new mysqli($servername, $username, $password, $database);
+
+    // Kiểm tra kết nối
+    if ($conn->connect_error) {
+        throw new Exception("Connection Failed: " . $conn->connect_error);
+    }
+
+    echo "Connection Success!";
+} catch (Exception $e) {
+    die("Connection Error: " . $e->getMessage());
+}
