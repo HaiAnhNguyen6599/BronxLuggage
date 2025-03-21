ce55a0f953fed96c9fb804054d9fcf90b80c8aa4
Hai Anh <nguyenhai.anh6599@gmail.com>
2025-03-21 23:50:39 +0700

    Fix Cart

--- pages/about.php
+++ pages/about.php
@@ -9,35 +9,44 @@ $user_id = $_SESSION['user_id'] ?? 0;
 <html lang="en">
 
 <head>
-  <!-- Meta Tags -->
-  <?php include '../includes/head.php'; ?>
-  <!-- End Meta Tags -->
+    <!-- Meta Tags -->
+    <?php include '../includes/head.php'; ?>
+    <!-- End Meta Tags -->
 </head>
 
 <body>
-  <!-- Topbar Start -->
-  <?php include '../includes/topbar.php'; ?>
-  <!-- Topbar End -->
+    <!-- Topbar Start -->
+    <?php include '../includes/topbar.php'; ?>
+    <!-- Topbar End -->
 
-  <!-- Navbar Start-->
-  <?php include '../includes/navbar.php'; ?>
-  <!-- Navbar End -->
-  <!-- Footer Start -->
-  <?php include '../includes/footer.php'; ?>
-  <!-- Footer End -->
+    <!-- Navbar Start-->
+    <?php include '../includes/navbar.php'; ?>
+    <!-- Navbar End -->
 
-  <!-- JavaScript Libraries -->
-  <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
-  <script src="../lib/easing/easing.min.js"></script>
-  <script src="../lib/owlcarousel/owl.carousel.min.js"></script>
+    <!-- Breadcrumb Start -->
+    <?php include '../includes/breadcumb.php'; ?>
+    <!-- Breadcrumb End -->
 
-  <!-- Contact Javascript File -->
-  <script src="../mail/jqBootstrapValidation.min.js"></script>
-  <script src="../mail/contact.js"></script>
 
-  <!-- Template Javascript -->
-  <script src="../js/main.js"></script>
+
+
+
+    <!-- Footer Start -->
+    <?php include '../includes/footer.php'; ?>
+    <!-- Footer End -->
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    <script src="../lib/easing/easing.min.js"></script>
+    <script src="../lib/owlcarousel/owl.carousel.min.js"></script>
+
+    <!-- Contact Javascript File -->
+    <script src="../mail/jqBootstrapValidation.min.js"></script>
+    <script src="../mail/contact.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="../js/main.js"></script>
 </body>
 
 </html>
